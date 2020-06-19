d22450a2a8ea6645d08689dfa8b561a8537cf503
scarsu <scarsu001@gmail.com>
2020-06-19 15:47:47 +0800

    feat pwa clock cache manage

--- source/pwa/clock/sw.js
+++ source/pwa/clock/sw.js
@@ -22,3 +22,29 @@ self.addEventListener('fetch', function(e) {
     })
   );
 });
+
+// 安装阶段跳过等待，直接进入 active
+var cacheName = "cache_v2"
+self.addEventListener('install', function (event) {
+    event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        Promise.all([
+            // 更新客户端
+            self.clients.claim(),
+
+            // 清理旧版本
+            caches.keys().then(function (cacheList) {
+                return Promise.all(
+                    cacheList.map(function (cacheName) {
+                        if (cacheName !== 'cache_v1') {
+                            return caches.delete(cacheName);
+                        }
+                    })
+                );
+            })
+        ])
+    );
+});
